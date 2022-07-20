eb5fe68b1f8e6a373bf381e058d7e6a487fc9cac
Adrian <adrianbertagnoli@gmail.com>
2022-07-20 15:16:40 +0200

    add logo

--- script_nm.js
+++ script_nm.js
@@ -3,4 +3,7 @@ $(".theme-switch").on("click", () => {
     var elem = document.getElementById("night_mode");
     if (elem.innerHTML=="Dark Mode") elem.innerHTML = "Light Mode";
     else elem.innerHTML = "Dark Mode";
+    var logo = document.getElementById("logo");
+    if (logo.getAttribute('data') == "logo2.svg") logo.data = "logo2_light.svg";
+    else logo.data = "logo2.svg";
   });
