043751b7d59ef8548f7fd926f229f3af5abe11e4
Adrian <adrianbertagnoli@gmail.com>
2022-07-21 09:01:43 +0200

    add dark CV

--- script_nm.js
+++ script_nm.js
@@ -6,4 +6,7 @@ $(".theme-switch").on("click", () => {
     var logo = document.getElementById("logo");
     if (logo.getAttribute('data') == "logo2.svg") logo.data = "logo2_light.svg";
     else logo.data = "logo2.svg";
+    var cv = document.getElementById("cv");
+    if (cv.getAttribute('data') == "Bertagnoli_CV.svg") cv.data = "Bertagnoli_CV_dark.svg";
+    else cv.data = "Bertagnoli_CV.svg";
   });
