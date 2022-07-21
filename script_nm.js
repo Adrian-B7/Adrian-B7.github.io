6bc79fe732c8fa79b0c3323919e913dc8d3497ad
Adrian <adrianbertagnoli@gmail.com>
2022-07-21 12:03:23 +0200

    new dark mode

--- script_nm.js
+++ script_nm.js
@@ -1,12 +1,23 @@
-$(".theme-switch").on("click", () => {
-    $("body").toggleClass("dark-mode");
-    var elem = document.getElementById("night_mode");
-    if (elem.innerHTML=="Dark Mode") elem.innerHTML = "Light Mode";
-    else elem.innerHTML = "Dark Mode";
-    var logo = document.getElementById("logo");
-    if (logo.getAttribute('data') == "logo2.svg") logo.data = "logo2_light.svg";
-    else logo.data = "logo2.svg";
-    var cv = document.getElementById("cv");
-    if (cv.getAttribute('data') == "Bertagnoli_CV.svg") cv.data = "Bertagnoli_CV_dark.svg";
-    else cv.data = "Bertagnoli_CV.svg";
-  });
+// $(".theme-switch").on("click", () => {
+//     $("body").classList.add('class');("dark-mode");
+//     var elem = document.getElementById("night_mode");
+//     if (elem.innerHTML=="Dark Mode") elem.innerHTML = "Light Mode";
+//     else elem.innerHTML = "Dark Mode";
+//     var logo = document.getElementById("logo");
+//     if (logo.getAttribute('data') == "logo2.svg") logo.data = "logo2_light.svg";
+//     else logo.data = "logo2.svg";
+//     var cv = document.getElementById("cv");
+//     if (cv.getAttribute('data') == "Bertagnoli_CV.svg") cv.data = "Bertagnoli_CV_dark.svg";
+//     else cv.data = "Bertagnoli_CV.svg";
+//   });
+
+if(document.getElementById('toggle-on').checked) {
+  document.body.classList.remove('dark-mode');
+  cv.data = "Bertagnoli_CV.svg";
+  logo.data = "logo2.svg";
+  
+}else{
+  document.body.classList.add('dark-mode');
+  cv.data = "Bertagnoli_CV_dark.svg";
+  logo.data = "logo2_light.svg";
+}
