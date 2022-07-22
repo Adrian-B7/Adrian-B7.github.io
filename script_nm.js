00e5576a860a3cb7367a15085afa616c87ee06f5
Adrian <adrianbertagnoli@gmail.com>
2022-07-22 11:19:39 +0200

    browser dark mode detection

--- script_nm.js
+++ script_nm.js
@@ -3,6 +3,8 @@ var cv = document.getElementById("cv")
 var profile = document.getElementById("profile")
 var logo = document.getElementById("logo")
 var icon = document.getElementById("icon")
+var icon = document.getElementById("icon")
+
 function nm(val) {
   if(val=="false") {
 document.body.classList.remove('dark-mode');
@@ -10,6 +12,8 @@ cv.data = "Bertagnoli_CV.svg";
 logo.data = "logo2.svg";
 icon.href = "logo2.png";
 profile.src = 'Profile_pink.png';
+document.documentElement.setAttribute('color-scheme', 'light');
+
 
 }
 else{
@@ -18,5 +22,6 @@ cv.data = "Bertagnoli_CV_dark.svg";
 logo.data = "logo2_light.svg";
 icon.href = "logo2_light.png";
 profile.src = 'Profile_blue.png';
+document.documentElement.setAttribute('color-scheme', 'dark');
 }
 }
