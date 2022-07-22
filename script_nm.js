58f09dcffee316cc627248382c9e93cd33659de8
Adrian <adrianbertagnoli@gmail.com>
2022-07-22 12:46:22 +0200

    dark mode browser support, fixed about page layout

--- script_nm.js
+++ script_nm.js
@@ -4,24 +4,35 @@ var profile = document.getElementById("profile")
 var logo = document.getElementById("logo")
 var icon = document.getElementById("icon")
 var icon = document.getElementById("icon")
+var color_scheme = document.getElementById("color-scheme")
 
+if(window.matchMedia &&
+  window.matchMedia('(prefers-color-scheme: dark)').matches){
+    document.getElementById('toggle-off').checked = true;
+    document.body.classList.add('dark-mode');
+    cv.src = "Bertagnoli_CV_dark.svg";
+    logo.src = "logo2_light.svg";
+    icon.href = "logo2_light.png";
+    profile.src = 'Profile_blue.png';
+    color_scheme.content = 'dark'
+  }
 function nm(val) {
   if(val=="false") {
 document.body.classList.remove('dark-mode');
-cv.data = "Bertagnoli_CV.svg";
-logo.data = "logo2.svg";
-icon.href = "logo2.png";
+cv.src = "Bertagnoli_CV.svg";
+logo.src = "logo2.svg";
+// icon.href = "logo2.png";
 profile.src = 'Profile_pink.png';
-document.documentElement.setAttribute('color-scheme', 'light');
+color_scheme.content = 'light'
 
 
 }
 else{
 document.body.classList.add('dark-mode');
-cv.data = "Bertagnoli_CV_dark.svg";
-logo.data = "logo2_light.svg";
-icon.href = "logo2_light.png";
+cv.src = "Bertagnoli_CV_dark.svg";
+logo.src = "logo2_light.svg";
+// icon.href = "logo2_light.png";
 profile.src = 'Profile_blue.png';
-document.documentElement.setAttribute('color-scheme', 'dark');
+color_scheme.content = 'dark'
 }
 }
