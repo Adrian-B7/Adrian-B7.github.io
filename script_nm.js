a1e939983a3fbd3c56fe74c5030802b0b5c6e7f4
Adrian <adrianbertagnoli@gmail.com>
2022-07-22 08:04:58 +0200

    added icon for light and dark mode

--- script_nm.js
+++ script_nm.js
@@ -2,11 +2,13 @@
 var cv = document.getElementById("cv")
 var profile = document.getElementById("profile")
 var logo = document.getElementById("logo")
+var icon = document.getElementById("icon")
 function nm(val) {
   if(val=="false") {
 document.body.classList.remove('dark-mode');
 cv.data = "Bertagnoli_CV.svg";
 logo.data = "logo2.svg";
+icon.href = "logo2.png";
 profile.src = 'Profile_pink.png';
 
 }
@@ -14,6 +16,7 @@ else{
 document.body.classList.add('dark-mode');
 cv.data = "Bertagnoli_CV_dark.svg";
 logo.data = "logo2_light.svg";
+icon.href = "logo2_light.png";
 profile.src = 'Profile_blue.png';
 }
 }
